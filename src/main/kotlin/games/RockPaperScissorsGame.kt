d8ad6ef1b1c834303940fc7106b8b7acd74e0913
Carolyn Biggar <carolyn.biggar@johnlewis.co.uk>
2022-12-05 21:07:16 +0000

    Day 2 - Find my score the new way in the Rock-Paper-Scissors League

--- src/main/kotlin/games/RockPaperScissorsGame.kt
+++ src/main/kotlin/games/RockPaperScissorsGame.kt
@@ -3,6 +3,8 @@ package games
 import games.Outcome.Draw
 import games.Outcome.Lose
 import games.Outcome.Win
+import games.RockPaperScissorsGame.getMyShape
+import games.RockPaperScissorsGame.play
 import games.Shape.Paper
 import games.Shape.Rock
 import games.Shape.Scissors
@@ -18,16 +20,16 @@ fun main() {
 class League(private val input: String) {
     fun myFinalScore(): Int = input
         .lines()
-        .sumOf { gameShapes ->
-            val (theirShape, myShape) = gameShapes.split(" ").map { Shape.fromLetter(it) }
-            RockPaperScissorsGame.getMyScore(theirShape, myShape)
-
+        .sumOf { game ->
+            val theirShape = Shape.fromLetter(game.substringBefore(" "))
+            val outcome = Outcome.fromLetter(game.substringAfter(" "))
+            RockPaperScissorsGame.getMyScore(getMyShape(theirShape, outcome), outcome)
         }
 }
 
 object RockPaperScissorsGame {
-    fun getMyScore(theirShape: Shape, myShape: Shape): Int {
-        return myShape.score + play(theirShape, myShape).score
+    fun getMyScore(myShape: Shape, outcome: Outcome): Int {
+        return myShape.score + outcome.score
     }
 
     fun play(theirShape: Shape, myShape: Shape): Outcome {
@@ -38,14 +40,35 @@ object RockPaperScissorsGame {
             myShape == theirShape -> Draw
             else -> Lose
         }
-
     }
+
+    fun getMyShape(theirShape: Shape, outcome: Outcome): Shape {
+        return when {
+            outcome == Win && theirShape == Scissors -> Rock
+            outcome == Win && theirShape == Rock -> Paper
+            outcome == Win && theirShape == Paper -> Scissors
+            outcome == Lose && theirShape == Scissors -> Paper
+            outcome == Lose && theirShape == Rock -> Scissors
+            outcome == Lose && theirShape == Paper -> Rock
+            else -> theirShape
+        }
+    }
+
 }
 
 enum class Outcome(val score: Int) {
     Win(6),
     Draw(3),
     Lose(0);
+
+    companion object {
+        fun fromLetter(letter: String): Outcome = when (letter) {
+            "X" -> Lose
+            "Y" -> Draw
+            "Z" -> Win
+            else -> throw Exception("Unknown outcome! Letter $letter")
+        }
+    }
 }
 
 enum class Shape(val score: Int) {
@@ -55,9 +78,9 @@ enum class Shape(val score: Int) {
 
     companion object {
         fun fromLetter(letter: String): Shape = when (letter) {
-            "A", "X" -> Rock
-            "B", "Y" -> Paper
-            "C", "Z" -> Scissors
+            "A" -> Rock
+            "B" -> Paper
+            "C" -> Scissors
             else -> throw Exception("Unknown shape! Letter $letter")
         }
     }
--- src/test/kotlin/RockPaperScissorsGameTest.kt
+++ src/test/kotlin/RockPaperScissorsGameTest.kt
@@ -3,6 +3,7 @@ import games.Outcome.*
 import games.RockPaperScissorsGame
 import games.Shape
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -10,54 +11,98 @@ class RockPaperScissorsGameTest {
     @ParameterizedTest(name = "Letter {0} is shape {1}")
     @CsvSource(
         "A,Rock",
-        "X,Rock",
         "B,Paper",
-        "Y,Paper",
         "C,Scissors",
-        "Z,Scissors",
     )
     fun `should return shape for letter`(letter: String, shape: Shape) {
         assertThat(Shape.fromLetter(letter)).isEqualTo(shape)
     }
 
-    @ParameterizedTest(name = "Winning combination my shape {1} over their shape {0}")
+    @ParameterizedTest(name = "Letter {0} is outcome {1}")
     @CsvSource(
-        "Scissors,Rock",
-        "Rock,Paper",
-        "Paper,Scissors",
+        "X,Lose",
+        "Y,Draw",
+        "Z,Win",
     )
-    fun `should return win result`(theirShape: Shape, myShape: Shape) {
-        assertThat(RockPaperScissorsGame.play(theirShape, myShape)).isEqualTo(Win)
+    fun `should return outcome for letter`(letter: String, outcome: Outcome) {
+        assertThat(Outcome.fromLetter(letter)).isEqualTo(outcome)
     }
 
-    @ParameterizedTest(name = "Draw with {0}")
-    @CsvSource(
-        "Scissors",
-        "Rock",
-        "Paper",
-    )
-    fun `should return draw when shapes are the same`(shape: Shape) {
-        assertThat(RockPaperScissorsGame.play(shape, shape)).isEqualTo(Draw)
-    }
+    @Nested
+    inner class FindResult {
 
-    @ParameterizedTest(name = "Losing combination their shape {0} over my shape {1}")
-    @CsvSource(
-        "Rock,Scissors",
-        "Paper,Rock",
-        "Scissors,Paper",
-    )
-    fun `should lose with any other combination`(theirShape: Shape, myShape: Shape) {
-        assertThat(RockPaperScissorsGame.play(theirShape, myShape)).isEqualTo(Lose)
+        @ParameterizedTest(name = "Winning combination my shape {1} over their shape {0}")
+        @CsvSource(
+            "Scissors,Rock",
+            "Rock,Paper",
+            "Paper,Scissors",
+        )
+        fun `should return win result`(theirShape: Shape, myShape: Shape) {
+            assertThat(RockPaperScissorsGame.play(theirShape, myShape)).isEqualTo(Win)
+        }
+
+        @ParameterizedTest(name = "Draw with {0}")
+        @CsvSource(
+            "Scissors",
+            "Rock",
+            "Paper",
+        )
+        fun `should return draw when shapes are the same`(shape: Shape) {
+            assertThat(RockPaperScissorsGame.play(shape, shape)).isEqualTo(Draw)
+        }
+
+        @ParameterizedTest(name = "Losing combination their shape {0} over my shape {1}")
+        @CsvSource(
+            "Rock,Scissors",
+            "Paper,Rock",
+            "Scissors,Paper",
+        )
+        fun `should lose with any other combination`(theirShape: Shape, myShape: Shape) {
+            assertThat(RockPaperScissorsGame.play(theirShape, myShape)).isEqualTo(Lose)
+        }
     }
 
     @ParameterizedTest(name = "{2} with {1} gives score {3}")
     @CsvSource(
-        "Rock,Scissors,Lose,3",
-        "Paper,Scissors,Win,9",
+        "Scissors,Lose,3",
+        "Scissors,Win,9",
     )
-    fun `should score`(theirShape: Shape, myShape: Shape, outcome: Outcome, score: Int) {
-        assertThat(RockPaperScissorsGame.play(theirShape, myShape)).isEqualTo(outcome)
-        assertThat(RockPaperScissorsGame.getMyScore(theirShape, myShape)).isEqualTo(score)
+    fun `should score`(myShape: Shape, outcome: Outcome, score: Int) {
+        assertThat(RockPaperScissorsGame.getMyScore(myShape, outcome)).isEqualTo(score)
+    }
+
+    @Nested
+    inner class FindMyShape {
+
+        @ParameterizedTest(name = "Draw: my shape is {0}, same as theirs")
+        @CsvSource(
+            "Scissors",
+            "Rock",
+            "Paper",
+        )
+        fun `should return same shape with a draw`(shape: Shape) {
+            assertThat(RockPaperScissorsGame.getMyShape(shape, Draw)).isEqualTo(shape)
+        }
+
+        @ParameterizedTest(name = "Win: my shape is {1} over their shape {0}")
+        @CsvSource(
+            "Scissors,Rock",
+            "Rock,Paper",
+            "Paper,Scissors",
+        )
+        fun `should return shape for win `(theirShape: Shape, myShape: Shape) {
+            assertThat(RockPaperScissorsGame.getMyShape(theirShape, Win)).isEqualTo(myShape)
+        }
+
+        @ParameterizedTest(name = "Lose: my shape is {1} to their {0}")
+        @CsvSource(
+            "Rock,Scissors",
+            "Paper,Rock",
+            "Scissors,Paper",
+        )
+        fun `should return shape for lose`(theirShape: Shape, myShape: Shape) {
+            assertThat(RockPaperScissorsGame.getMyShape(theirShape, Lose)).isEqualTo(myShape)
+        }
     }
 
 }
