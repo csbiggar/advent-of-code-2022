f033655197f9eb195f181eef3f4f0e2eaf23c090
Carolyn Biggar <carolyn.biggar@johnlewis.co.uk>
2022-12-11 23:24:19 +0000

    Day 5 - Move crates around

---
+++ src/main/kotlin/supplies/CrateStacking.kt
@@ -0,0 +1,78 @@
+package supplies
+
+import rucksacks.CaloriesCalculator
+
+fun main() {
+    val input = CaloriesCalculator::class.java.getResource("/day-5.txt")!!.readText()
+
+    val stacks = extractStartingStacks(input)
+    val instructions = extractInstructions(input)
+
+    println(stacks)
+    println(instructions)
+    stacks.rearrangeCrates(instructions)
+
+    println(getTopCrate(stacks).joinToString(""))
+
+}
+
+
+fun List<Stack>.rearrangeCrates(instructions: List<CratesMove>) {
+    instructions.forEach { move ->
+        val fromStack = single { it.id == move.fromStack }
+        val toStack = single { it.id == move.toStack }
+
+        val cratesToMove = fromStack.crates.takeLast(move.numberToMove)
+
+        toStack.crates.addAll(cratesToMove.reversed())
+        fromStack.crates.removeLast(move.numberToMove)
+    }
+}
+
+fun getTopCrate(stacks: List<Stack>): List<Char> {
+    return stacks.map { it.crates.last() }
+}
+
+private fun MutableList<Char>.removeLast(howMany: Int) {
+    repeat(howMany) {
+        this.removeLast()
+    }
+}
+
+fun extractStartingStacks(input: String): List<Stack> {
+    val rows = input.substringBefore("\n\n").lines().reversed()
+
+    val stacks = rows.first()
+        .trim()
+        .split("   ")
+        .map { Stack(it.toInt(), mutableListOf()) }
+
+    rows.drop(1).forEach { verticalPosition ->
+        verticalPosition
+            .windowed(4, 4, partialWindows = true)
+            .forEachIndexed { index, crateEntry ->
+                val crate = crateEntry[1]
+                if (crate != ' ') {
+                    stacks.single { it.id == index + 1 }.crates.add(crate)
+                }
+            }
+    }
+
+    return stacks
+}
+
+fun extractInstructions(input: String): List<CratesMove> {
+    return input.substringAfter("\n\n")
+        .lines()
+        .map {
+            CratesMove(
+                fromStack = it.substringAfter("from ").substringBefore(" to").toInt(),
+                toStack = it.substringAfterLast(" ").toInt(),
+                numberToMove = it.substringAfter("move ").substringBefore(" from").toInt()
+            )
+        }
+}
+
+data class CratesMove(val fromStack: Int, val toStack: Int, val numberToMove: Int)
+
+data class Stack(val id: Int, val crates: MutableList<Char>)
---
+++ src/test/kotlin/supplies/CrateStackingTest.kt
@@ -0,0 +1,104 @@
+package supplies
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CrateStackingTest {
+
+    private val example = """    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2""".trimIndent()
+
+    @Test
+    fun `should rearrange crates`() {
+        val stacks = listOf(
+            Stack(1, mutableListOf('Z', 'N')),
+            Stack(2, mutableListOf('M', 'C', 'D')),
+            Stack(3, mutableListOf('P')),
+        )
+
+        val instructions = listOf<CratesMove>(
+            CratesMove(fromStack = 2, toStack = 1, numberToMove = 1),
+            CratesMove(fromStack = 1, toStack = 3, numberToMove = 3),
+            CratesMove(fromStack = 2, toStack = 1, numberToMove = 2),
+            CratesMove(fromStack = 1, toStack = 2, numberToMove = 1),
+        )
+
+        stacks.rearrangeCrates(instructions)
+
+        assertThat(stacks).containsExactly(
+            Stack(1, mutableListOf('C')),
+            Stack(2, mutableListOf('M')),
+            Stack(3, mutableListOf('P', 'D', 'N', 'Z')),
+        )
+    }
+
+    @Test
+    fun `should give the top crate of each stack`() {
+        val stacks = listOf(
+            Stack(1, mutableListOf('Z', 'N')),
+            Stack(2, mutableListOf('M', 'C', 'D')),
+            Stack(3, mutableListOf('P')),
+        )
+
+        val result = getTopCrate(stacks)
+
+        assertThat(result).containsExactly('N', 'D', 'P')
+
+    }
+
+    @Test
+    fun `should extract starting stack`() {
+        val stacks = extractStartingStacks(example)
+        assertThat(stacks).containsExactly(
+            Stack(1, mutableListOf('Z', 'N')),
+            Stack(2, mutableListOf('M', 'C', 'D')),
+            Stack(3, mutableListOf('P')),
+        )
+    }
+
+    @Test
+    fun `should extract starting stacks when middle stack has no crate`() {
+
+        val missingCrate = """[Z]     [P]
+ 1   2   3 
+
+move 1 from 2 to 1"""
+
+        val stacks = extractStartingStacks(missingCrate)
+        assertThat(stacks).containsExactly(
+            Stack(1, mutableListOf('Z')),
+            Stack(2, mutableListOf()),
+            Stack(3, mutableListOf('P')),
+        )
+    }
+
+    @Test
+    fun `should extract instructions`() {
+        val instructions = extractInstructions(example)
+        assertThat(instructions).containsExactly(
+            CratesMove(fromStack = 2, toStack = 1, numberToMove = 1),
+            CratesMove(fromStack = 1, toStack = 3, numberToMove = 3),
+            CratesMove(fromStack = 2, toStack = 1, numberToMove = 2),
+            CratesMove(fromStack = 1, toStack = 2, numberToMove = 1),
+        )
+    }
+
+    @Test
+    fun `should handle larger numbers in instructions`() {
+        val bigNumbers = """
+
+move 241 from 456 to 1"""
+
+        val instructions = extractInstructions(bigNumbers)
+        assertThat(instructions).containsExactly(
+            CratesMove(fromStack = 456, toStack = 1, numberToMove = 241),
+        )
+    }
+}
